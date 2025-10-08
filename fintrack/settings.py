da277befe65901de6433ca6a484f69355a4b83ba
451suman <sumanmushyakhwo@gmail.com>
2025-10-08 19:13:54 +0545

    modify setting config

--- fintrack/settings.py
+++ fintrack/settings.py
@@ -2,39 +2,15 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import platform
-def load_env(os_name):
-    """Loads environment variables based on the operating system using suitable methods.
+import platform
 
-    Args:
-        os_name: The name of the operating system (e.g., 'linux', 'windows', 'darwin').
-
-    Raises:
-        ValueError: If the provided OS name is not supported.
-    """
-    if os_name in ("linux", "darwin"):
-        # Assuming a `.env` file for Linux and macOS
-        load_dotenv()
-    elif os_name == "windows":
-        # Assuming a `.env` file for Windows
-        load_dotenv(dotenv_path=os.path.join(os.environ["SYSTEMROOT"], ".env"))
-    else:
-        raise ValueError(f"Unsupported operating system: {os_name}")
-
-
-# Get the operating system name
-os_name = platform.system().lower()
-
-
-# Load environment variables based on OS
-load_env(os_name)
-
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-me-please-1234567890'
-DEBUG = False
-ALLOWED_HOSTS = ["suman967696.pythonanywhere.com"]
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', True)
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -105,4 +81,6 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
 
-CSRF_TRUSTED_ORIGINS = ["https://suman967696.pythonanywhere.com"]
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://127.0.0.1:8000").split(",")
+print("ALLOWED_HOSTS =", ALLOWED_HOSTS)
+print("CSRF_TRUSTED_ORIGINS =", CSRF_TRUSTED_ORIGINS)
